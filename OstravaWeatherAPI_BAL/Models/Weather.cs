64a356c208985055cfe25712544998f828cd0db3
basileia <jane.novak@centrum.cz>
2024-05-01 21:26:52 +0200

    fill DailyWeather if available

--- OstravaWeatherAPI_BAL/Models/Weather.cs
+++ OstravaWeatherAPI_BAL/Models/Weather.cs
@@ -5,21 +5,22 @@ namespace OstravaWeatherAPI_BAL.Models
     {
         public int Id { get; set; }
         public Daily? Daily { get; set; }
-        
+
         public DailyWeather ToDailyWeather()
-        {  
-            DailyWeather dailyweather = new DailyWeather();
-            dailyweather.Id = Id;
-            dailyweather.TemperatureMax = Daily.Temperature_2m_max[0];
-            dailyweather.TemperatureMin = Daily.Temperature_2m_min[0];
-            dailyweather.RainSum = Daily.Rain_sum[0];
-            
+        {
+            DailyWeather dailyWeather = new DailyWeather
+            {
+                Id = Id
+            };
+
+            fillDailyWeather(Daily, dailyWeather);
+
             if (stringToDateOnly(Daily.Time[0]).Error == null)
-            {                
-                dailyweather.Date = stringToDateOnly(Daily.Time[0]).Value;
+            {
+                dailyWeather.Date = stringToDateOnly(Daily.Time[0]).Value;
             }
 
-            return dailyweather;
+            return dailyWeather;
         }
 
         private Result<DateOnly, Error> stringToDateOnly(string dateString)
@@ -31,6 +32,21 @@ namespace OstravaWeatherAPI_BAL.Models
                 return date;
             }
             return WeatherErrors.DateNotConverted;
+        }
+
+        private void fillIfAvailable(List<float> data, Action<float> fillAction)
+        {
+            if (data != null && data.Count > 0)
+            {
+                fillAction(data[0]);
+            }
+        }
+
+        private void fillDailyWeather(Daily dailyData, DailyWeather dailyWeather)
+        {
+            fillIfAvailable(dailyData.Temperature_2m_max, value => dailyWeather.TemperatureMax = value);
+            fillIfAvailable(dailyData.Temperature_2m_min, value => dailyWeather.TemperatureMin = value);
+            fillIfAvailable(dailyData.Rain_sum, value => dailyWeather.RainSum = value);
         }
     }
 }
